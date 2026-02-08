fab1bdbffecca36af76f26303b64af7c36d9dd8e
Merctxt <giovannihettwer2@gmail.com>
2026-02-08 17:36:31 -0300

    fix: cors

--- src/main/java/com/dio/pix/config/OpenApiConfig.java
+++ src/main/java/com/dio/pix/config/OpenApiConfig.java
@@ -1,4 +1,5 @@
 package com.dio.pix.config;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
@@ -7,22 +8,17 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
+
 @Configuration
 public class OpenApiConfig {
 
-    @Value("${ENV:dev}")
-    private String environment;
-
-    @Value("${SERVER_PORT:8080}")
-    private String serverPort;
+    @Value("${API_URL:http://localhost:8080}")
+    private String apiUrl;
 
     @Bean
     public OpenAPI customOpenAPI() {
-        String serverUrl = "dev".equals(environment)
-            ? "http://localhost:" + serverPort
-            : "https://pix.giovannidev.com";
-
         return new OpenAPI()
                 .info(new Info()
                         .title("API REST PIX")
@@ -37,8 +33,8 @@ public class OpenApiConfig {
                                 .url("https://www.apache.org/licenses/LICENSE-2.0")))
                 .servers(List.of(
                         new Server()
-                                .url(serverUrl)
-                                .description(environment.equals("dev") ? "Desenvolvimento" : "Produção")
+                                .url(apiUrl)
+                                .description("Servidor")
                 ));
     }
 }
